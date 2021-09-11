7a3f93ce14c5f663afdf35b9dafa1084caf78ef3
Fredrik Strömbäck <stromback@pop-os.localdomain>
2021-09-12 01:01:12 +0200

    Now express serves a simple message

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 "use strict"
 
-console.log('Hello Wrold!')
+const express = require("express")
+
+const app = express()
+
+app.get('/', (req, res, next) => {
+  res.send('Hello From Express!')
+})
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000')
+})
