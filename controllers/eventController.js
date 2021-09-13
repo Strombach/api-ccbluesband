567a24428adda28f84d97164a814b2ed89c27ac4
Fredrik Strömbäck <fredde.stromback@gmail.com>
2021-09-13 02:53:01 +0200

    adds handler for getting single event

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -6,6 +6,10 @@ eventController.getAllEvents = (req, res, next) => {
   res.send('Hello From eventController')
 }
 
+eventController.getSingleEvent = (req, res, next) => {
+  res.send('Hello from getSingleEvent')
+}
+
 eventController.addNewEvent = (req, res, next) => {
   res.send('New Event added!')
 }
--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -5,8 +5,10 @@ const router = express.Router()
 
 const controller = require('../controllers/eventController')
 
-router.get("/", controller.getAllEvents)
+router.get('/', controller.getAllEvents)
 
-router.post("/", controller.addNewEvent)
+router.get('/:eventId', controller.getSingleEvent)
+
+router.post('/', controller.addNewEvent)
 
 module.exports = router
