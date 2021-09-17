39716d324aacd4ccfd2c7d61501c0302460b2704
Fredrik Strömbäck <fredde.stromback@gmail.com>
2021-09-17 02:23:41 +0200

    adds 404-catcher

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express()
 app.use('/events', require('./routes/eventRoute'))
 app.use('/', require('./routes/indexRoute'))
 
+// Catch 404
+app.use((req, res, next) => {
+  res.status(404).json({
+    "status": 404,
+    "message": 'Nothing here'
+  })
+})
+
 app.listen(5000, () => {
   console.log('Server listening on port 5000')
 })
