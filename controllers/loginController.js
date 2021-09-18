63305b89fe5e89c9b15b0e1e6ed41ae01b50582b
Fredrik Strömbäck <fredde.stromback@gmail.com>
2021-09-19 01:06:40 +0200

    replaces double quotes with single quotes

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const User = require('../models/userSchema')
 
 const loginController = {}
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,10 +1,10 @@
-"use strict"
+'use strict'
 
 const express = require('express')
 const router = express.Router()
 
 const controller = require('../controllers/indexController')
 
-router.get("/", controller.getIndex)
+router.get('/', controller.getIndex)
 
 module.exports = router
