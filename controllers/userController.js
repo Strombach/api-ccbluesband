4fcc673290be33af7c05f8dbff1000356c772027
Fredrik Strömbäck <strombach@pop-os.localdomain>
2021-09-19 02:47:13 +0200

    now registering new users works

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,8 +14,31 @@ userController.userLogin = (req, res, next) => {
   res.send('Logged in!')
 }
 
-userController.userRegister = (req, res, next) => {
-  res.send('User registered')
+/**
+ * Handles post-request for registering new users.
+ * @param {*} req The request object.
+ * @param {*} res The response object.
+ * @param {*} next The next function.
+ */
+userController.userRegister = async (req, res, next) => {
+  try {
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password
+    })
+
+    let dbResponse = await newUser.save()
+
+    res.send(dbResponse)
+  } catch (error) {
+    res.status(422).json({
+      errorMessage: error.message,
+      fullError: error
+    })
+  }
 }
 
 module.exports = userController
--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
   username: {
     type: String,
     required: true,
